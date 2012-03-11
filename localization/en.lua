cae7af56b4aeda6ee44797cc7889f679dabb7a44
Jaliborc <contact@jaliborc.com>
2012-03-11 12:39:15 +0000

    Money frame is ready for localization and now properly inherits from core class

--- localization/en.lua
+++ localization/en.lua
@@ -8,6 +8,10 @@ local L = LibStub('AceLocale-3.0'):NewLocale('Bagnon-GuildBank', 'enUS', true)
 L.Title = [[%s's Guild Bank]]
 L.Log1 = 'Transaction Log'
 L.Log3 = 'Tab Information'
+L.TipFunds = 'Guild Funds'
+L.TipDeposit = '<Left Click> to deposit.'
+L.TipWithdrawRemaining = '<Right Click> to withdraw (%s remaining).'
+L.TipWithdraw = '<Right Click> to withdraw.'
 
 
 -- Automatically localized - do not translate!
