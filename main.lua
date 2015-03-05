c908e5a260f162418df05a14b68e2b31fcbc1ec7
João Cardoso <contact@jaliborc.com>
2015-03-04 22:28:30 -0800

    Updated to prevent loading issues

--- main.lua
+++ main.lua
@@ -6,17 +6,16 @@
 local GuildBank = Bagnon:NewModule('GuildBank', 'AceEvent-3.0')
 
 function GuildBank:OnEnable()
-	self:RegisterEvent('GUILDBANKFRAME_OPENED')
-	self:RegisterEvent('GUILDBANKFRAME_CLOSED')
+	self:RegisterEvent('GUILDBANKFRAME_CLOSED', 'OnClosed')
 end
 
-function GuildBank:GUILDBANKFRAME_OPENED()
+function GuildBank:OnOpen()
 	Bagnon.Cache.AtGuild = true
 	Bagnon:ShowFrame('guild')
 	QueryGuildBankTab(GetCurrentGuildBankTab())
 end
 
-function GuildBank:GUILDBANKFRAME_CLOSED()
+function GuildBank:OnClosed()
 	Bagnon.Cache.AtGuild = nil
 	Bagnon:HideFrame('guild')
 end
